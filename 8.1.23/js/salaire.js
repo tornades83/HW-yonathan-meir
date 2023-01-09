c08d7824a149900cf7a3910f4b2d2f6bbd1fcf60
yonathan roos <meirroos@gmail.com>
2023-01-09 08:59:22 +0200

    8.1.23

---
+++ 8.1.23/js/salaire.js
@@ -0,0 +1,37 @@
+var mois = 1;
+var montant = 0;
+var loyerDu = 0;
+var gift = false;
+var loyer = solde;
+var salaire = solde;
+var solde = solde + salaire;
+if (solde >= loyer) {
+    var solde = solde - loyer;
+} else {
+    var loyerDu = loyer;
+} alert("janvier")
+var mois = mois ++;
+var solde = solde + salaire / 2;
+if (solde = loyerDu) {
+    var solde = solde - loyerDu;
+}
+var loyer = loyer + 200;
+if (solde <= loyer) {
+    var solde = solde - loyer;
+} else {
+    var loyerDu = loyerDu + loyer;
+} alert("fevrier")
+var solde = solde - 500;
+var mois = mois ++;
+if(solde , 0){
+    var gift = true;
+    var montant =solde * -1;
+}else{
+    var montant = solde;
+}alert("mars")
+var solde = 0;
+alert(mois , montant , loyer , gift);
+
+
+
+
