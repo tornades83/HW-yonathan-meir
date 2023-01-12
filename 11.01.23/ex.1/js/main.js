253b9e706f880c7dc26ceb08fb5a0aaf2840563b
yonathan roos <meirroos@gmail.com>
2023-01-12 09:01:04 +0200

    12.1.23

---
+++ 11.01.23/ex.1/js/main.js
@@ -0,0 +1,28 @@
+var etudiant = [{ nom: "meir", note: 90 }, { nom: "yonathan", note: 100 }, { nom: "ephraim", note: 50 }, { nom: "avi", note: 70 }, { nom: "chay", note: 75 }, { nom: "dan", note: 90 }, { nom: "avraham", note: 80 }, { nom: "israk", note: 56 }, { nom: "reouven", note: 72 }, { nom: "chimon", note: 48 }, { nom: "levi", note: 20 }, { nom: "yeouda", note: 78 }, { nom: "isahah", note: 69 }, { nom: "zevouloun", note: 54 }, { nom: "acher", note: 88 }, { nom: "naftali", note: 90 }, { nom: "yosef", note: 70 }, { nom: "biniyamin", note: 68 }, { nom: "gad", note: 45 }, { nom: "yahakov", note: 55 }, { nom: "israel", note: 74 }, { nom: "gamliel", note: 71 }, { nom: "azriel", note: 78 }, { nom: "elya", note: 90 }, { nom: "chemouel", note: 20 }, { nom: "david", note: 23 }, { nom: "ouziel", note: 78 }, { nom: "chelomo", note: 99 }, { nom: "mihael", note: 97 }, { nom: "gabriel", note: 45 }, { nom: "refael", note: 100 }, { nom: "netanel", note: 100 }, { nom: "nathan", note: 85 }, { nom: "eli", note: 35 }, { nom: "hillel", note: 45 }, { nom: "eliezer", note: 78 }, { nom: "shoam", note: 82 }, { nom: "aviel", note: 56 }, { nom: "", note: 78 }, { nom: "nadav", note: 70 }, { nom: "ranan", note: 90 }, { nom: "karmi", note: 10 }, { nom: "sadok", note: 05 }, { nom: "avive", note: 45 }, { nom: "ariel", note: 45 }, { nom: "arier", note: 82 }, { nom: "avichay", note: 82 }, { nom: "haniel", note: 45 }, { nom: "yohay", note: 48 }, { nom: "dov", note: 45 }]
+var note = "";
+var moyenne = "";
+note1 <= 70;
+note2 > 70;
+if (moyenne >= 70) {
+    var note1 = moyenne;
+    for (let i = 0; i < note1.length; i++) {
+        const moyenne = note1[i];
+        var temp = x % 10; 
+        sum  += temp; 
+        x = (x - temp) / 10;
+        var note1 ="";
+
+    }
+    alert("moyen 70 >= " + note1 )
+}
+ else (moyenne < 70)
+    var note2 = moyenne;
+    for (let i = 0; i < note2.length; i++) {
+        const moyenne = note2[i];
+        var temp = x % 10;
+        sum += temp; 
+        x = (x - temp) / 10;
+        var note2 ="";
+    }
+alert("moyen 70 < " + note2 )
+
