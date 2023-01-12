4622b78bd175843f341ad87a758469bdbaa3771f
yonathan roos <meirroos@gmail.com>
2023-01-12 09:40:26 +0200

    12.01.23

---
+++ 11.01.23/ex2/js/main.js
@@ -0,0 +1,12 @@
+var students =["meir" , "yonathan" , "david" , "avi" , "yosef"]
+
+var result = students.push()
+
+alert(result)
+
+var result = students.sort()
+
+alert(result)
+
+var result = students.join(",")
+alert(result)
