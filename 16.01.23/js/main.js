0db90383314579c61fb461330a94b09dbf04f7ce
yonathan roos <meirroos@gmail.com>
2023-01-17 16:42:18 +0200

    17.01.23

---
+++ 16.01.23/js/main.js
@@ -0,0 +1,11 @@
+const emailAdress = {}
+var email = prompt("enter email");
+function email(gmail) {
+   for (let i = 0; i < gmail.length; i++) {
+    const gmail = props[i].split(".");
+    str = str.replaceAll("", ".");
+    return "this is the correct email address:" + email ;
+    
+   }
+}
+console.log(email);
